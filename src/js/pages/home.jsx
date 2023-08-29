738f870500ee935e48d257f3ec7d2983fea89794
SamuelCarmona83 <samuel.carmona.rodrigz@gmail.com>
2023-08-29 15:06:54 -0400

    :alembic: testing

---
+++ src/js/pages/home.jsx
@@ -0,0 +1,12 @@
+//import { useContext } from "react";
+//import { Context } from "../store/appContext";
+
+export const Home = () => {
+  //const { store, actions } = useContext(Context);
+
+  return (
+    <div className="text-center mt-5">
+      <h1>Hello Rigo!!</h1>
+    </div>
+  );
+};
